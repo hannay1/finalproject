a46bf58b90cc002c7ffa08ea017cc3a55b3f2a83
hannay1 <hannay11211@live.com>
2015-10-09 21:06:27 -0600

    message1

--- tagclient.js
+++ tagclient.js
@@ -15,17 +15,26 @@ bgImage.onload = function () {
 	};
 
 
-//render player1 image
-var bgImage = new Image();
-bgImage.src = "images/red.png"
-bgImage.onload = function () {
-	ctx.drawImage(bgImage, w/2, h/2);
+//player1 image (to be loaded later)
+var p1loaded = false;
+var p1img = new Image();
+p1img.src = "images/red.png";
+p1img.onload = function () {
+	p1loaded = true;
 	};
 
 
+//player2 image (to be loaded later)
+var p2loaded = false;
+var p2img = new Image();
+p2img.src = "images/blue.png";
+p2img.onload = function () {
+	p2loaded = true;
+};
 
 var points = []; //an empty array to store points
 var team = [];
+var flag = {};
 var possteams =["red", "blue"];
 
 function players (speed, isflag, team, points) 
@@ -39,13 +48,30 @@ function players (speed, isflag, team, points)
 var r = possteams[0];
 var b = possteams[1];
 
-var player1 = new players(256, false, r, 0);
+var player1 = new players(300, false, r, 0);
 team.push(player1);
 
-var player2 = new players(256, false, b, 0);
+var player2 = new players(300, false, b, 0);
 team.push(player2);
 
 console.log(team);
 
+var press = {};
 
-//render player 1
+addEventListener("pressed", function (arg)
+{
+	press[arg.keyCode];
+	console.log("key was pressed");
+}, false);
+
+addEventListener("unpressed", function (arg)
+{
+	delete press[arg.keyCode];
+	console.log("key was released");
+}, false);
+
+
+
+
+
+
