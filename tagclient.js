1ac71585e2bbb46bd57fd1db6b8b04219f1e9463
hannay1 <hannay11211@live.com>
2015-10-11 17:23:15 -0600

    message

--- tagclient.js
+++ tagclient.js
@@ -1,55 +1,32 @@
 var canvas = document.createElement("canvas");
 var ctx = canvas.getContext("2d");
-canvas.width = 640;
-canvas.height = 480;
+canvas.width = 1280;
+canvas.height = 720;
 document.body.appendChild(canvas);
 
 var w = canvas.width;
 var h = canvas.height;
 
-// render background image
-<<<<<<< HEAD
-var bgloaded = false;
-var bgimg = new Image();
-bgimg.src = "images/background.jpg"
-bgimg.onload = function () {
-	bgloaded = true;
-	};
-=======
-var bgImage = new Image();
-bgImage.src = "images/background.jpg";
-bgImage.onload = function() {
-    ctx.drawImage(bgImage, 0, 0);
-};
->>>>>>> f49e0553d77bf111f7ae31f18e8cd093deb921c8
 
+function imgloaded (loaded, img, source) {
+	this.loaded = loaded;
+	this.img = img;
+	this.img.src = source;
+	this.img.onload = function () {
+		loaded = true;
+	}
 
-//player1 image (to be loaded later)
-var p1loaded = false;
-var p1img = new Image();
-p1img.src = "images/red.png";
-p1img.onload = function() {
-    p1loaded = true;
 };
 
-//player2 image (to be loaded later)
-var p2loaded = false;
-var p2img = new Image();
-p2img.src = "images/blue.png";
-p2img.onload = function() {
-    p2loaded = true;
-};
+var bg = new imgloaded(true, new Image(), "images/background.jpg" );
+var p1 = new imgloaded(true, new Image(), "images/red.png" );
+var p2 = new imgloaded(true, new Image(), "images/blue.png" );
+var flg = new imgloaded(true, new Image(), "images/flag.png" );
+var bgoal = new imgloaded(true, new Image(), "images/goal1.png");
+var rgoal = new imgloaded(true, new Image(), "images/goal2.png");
 
-<<<<<<< HEAD
-//flag image (to be loaded later)
-var flagReady = false;
-var flagImage = new Image();
-flagImage.onload = function () {
-	flagReady = true;
-};
-flagImage.src = "images/flag.png";
-
-var points =""; //an empty variable to store points
+var bluegoal = {};
+var redgoal = {};
 var team = [];
 var flag = {};
 var possteams =["red", "blue"];
@@ -65,49 +42,13 @@ function players (speed, isflag, team, points)
 	this.points = points;
 }
 
-
-=======
-var points = [];
-var team = [];
-var flag = {};
-var possteams = ["red", "blue"];
-
-function players(speed, isflag, team, points, xpos, ypos) {
-    this.speed = speed;
-    this.isflag = isflag;
-    this.team = team;
-    this.points = points;
-    this.xpos = xpos;
-    this.ypos = ypos;
-}
-var r = possteams[0];
-var b = possteams[1];
->>>>>>> f49e0553d77bf111f7ae31f18e8cd093deb921c8
-
-var player1 = new players(300, false, r, 0, 0, 0);
+var player1 = new players(200, false, r, 0);
 team.push(player1);
 
-var player2 = new players(300, false, b, 0, 100, 100);
+var player2 = new players(200, false, b, 0);
 team.push(player2);
 
 console.log(team);
-
-<<<<<<< HEAD
-/*
-function teamSort (players)
-{
-	for (i=0; i<team.length; i++)
-	{
-	if(players[i].team = r)
-	{
-		//append 
-	}else 
-	{
-
-	}
-	}
-}
-*/
 
 var press = {};
 
@@ -124,74 +65,129 @@ addEventListener("keyup", function (e)
 }, false);
 
 
-var positions = function () {	
-player1.x = 32 + Math.floor((Math.random() * (w - 64)));
-player1.y = 32 + Math.floor((Math.random() * (h - 64)));
-console.log("player 1 is at"  + player1.x + " " + player1.y)
+var positions = function () 
+{	
 
-player2.x = 32 + Math.floor((Math.random() * (w - 64)));
-player2.y = 32 + Math.floor((Math.random() * (w - 64)));
-console.log("player 2 is at " + player2.x + " " + player2.y)
+player1.x = (w / 2) - 35;
+player1.y = 640;
 
-flag.x = w / 2;
-flag.y = h / 2;
+player2.x = (w / 2) - 35;
+player2.y = 30;
+
+flag.x = (w / 2);
+flag.y = (h / 2);
+
+bluegoal.x = (w / 2) - 30;
+bluegoal.y = 0;
+
+redgoal.x = (w / 2) - 30;
+redgoal.y = 690;
+
 }
 
-
 var reload = function (arg1) 
-{
-	if (38 in press) { // Player holding up
+{ //need to implement some way to sort input from player1/player2 keyboards later
+	
+
+	if (38 in press) { // Player 1 holding up
 		player1.y -= player1.speed * arg1;
 	}
-	if (40 in press) { // Player holding down
+	if (40 in press) { // Player  1holding down
 		player1.y += player1.speed * arg1;
 	}
-	if (37 in press) { // Player holding left
+	if (37 in press) { // Player 1 holding left
 		player1.x -= player1.speed * arg1;
 	}
-	if (39 in press) { // Player holding right
+	if (39 in press) { // Player 1 holding right
 		player1.x += player1.speed * arg1;
 	}
 
 
 
-if (87 in press) { // Player holding up
+if (87 in press) { // Player 2 holding up
 		player2.y -= player2.speed * arg1;
 	}
-	if (83 in press) { // Player holding down
+	if (83 in press) { // Player 2 holding down
 		player2.y += player2.speed * arg1;
 	}
-	if (65 in press) { // Player holding left
+	if (65 in press) { // Player 2 holding left
 		player2.x -= player2.speed * arg1;
 	}
-	if (68 in press) { // Player holding right
+	if (68 in press) { // Player 2 holding right
 		player2.x += player2.speed * arg1;
-	}
-//need to implement flag carrying ability for player1, player2	
+	}	
+
+checkifRisflag();
+checkifBisflag();
+//checkifRscore();
+//checkifBscore();
+
 };
 
-var begin = function () {
-	if(bgloaded === true)
+function checkifRisflag () 
+{
+	if(
+	player1.x <= (flag.x + 32)
+		&& flag.x <= (player1.x + 32)
+		&& player1.y <= (flag.y + 32)
+		&& flag.y <= (player1.y + 32)
+		
+	)
 	{
-		ctx.drawImage(bgimg, 0, 0);
+	player1.isflag = true;
+	console.log("player1 is carrying flag ", player1.isflag);
+	player1.x = player1.x; //keeps p1 moving after flag capture
+	player1.y = player1.y;
+	//need to implement way of removing flag until point is scored
+
+} 
+};
+
+function checkifBisflag()
+{
+	if(
+	player2.x <= (flag.x + 32)
+		&& flag.x <= (player2.x + 32)
+		&& player2.y <= (flag.y + 32)
+		&& flag.y <= (player2.y + 32)
+		
+	) {
+	player2.isflag = true;
+	console.log("player2 is carrying flag ", player2.isflag);
+	player2.x = player2.x; //keeps p2 moving after flag capture
+	player2.y = player2.y;
+	
+}
+};
+var begin = function () {
+	if(bg.loaded === true)
+	{
+		ctx.drawImage(bg.img, 0, 0);
 	}
 
-	if(p1loaded === true) {
-		ctx.drawImage(p1img, player1.x, player1.y);
+	if(p1.loaded === true) {
+		ctx.drawImage(p1.img, player1.x, player1.y);
 	}
-	if(p2loaded === true) {
-		ctx.drawImage(p2img, player2.x, player2.y);
+	if(p2.loaded === true) {
+		ctx.drawImage(p2.img, player2.x, player2.y);
 	}
-	if(flagReady === true){
-		ctx.drawImage(flagImage, flag.x, flag.y);
+	if(flg.loaded === true){
+		ctx.drawImage(flg.img, flag.x, flag.y);
+	}
+	if(bgoal.loaded === true){
+		ctx.drawImage(bgoal.img, bluegoal.x, bluegoal.y);
+	}
+	if(rgoal.loaded === true){
+		ctx.drawImage(rgoal.img, redgoal.x, redgoal.y);
 	}
 };
 
-var main = function () {
+var main = function () { //main function. 
 	var now = Date.now();
+	//console.log(now)
 	var delta = now - then;
 
-	reload(delta / 1000);
+	reload(delta / 500);
 	begin();
 
 	then = now;
@@ -201,85 +197,6 @@ var main = function () {
 
 
 
-var then = Date.now();
+var then = Date.now(); //in between this and main(), the difference will be saved in var then for use the next time main is called
 positions();
 main();
-
-
-
-
-=======
-function init() {
-	//ToDO -spawn players on opposite sides
-    if (p1loaded) {
-        ctx.drawImage(p1img, player1.xpos, player1.ypos);
-    }
-    if (p2loaded) {
-        ctx.drawImage(p1img, player2.xpos, player2.ypos);
-    }
-}
->>>>>>> f49e0553d77bf111f7ae31f18e8cd093deb921c8
-
-//player1 movement
-canvas.addEventListener("pressed", movePlayer1, false) {
-
-}
-//player 2 movement keycodes: w =87,a=65, s=83, d=65
-
-//arrow keys -needs border check
-function movePlayer1(evt) {
-    switch (evt.keyCode) {
-        //lefts
-        case 37:
-            {
-                player1.posx -= player1.speed;
-                break;
-            }
-            //right
-        case 39:
-            {
-                player1.posx += player1.speed;
-                break;
-            }
-            //up
-        case 38:
-            {
-                player1.posy -= player1.speed;
-                break;
-            }
-
-            //down
-        case 40:
-            {
-                player1.posy += player1.speed; {
-                break;
-                }
-
-            }
-           redraw();
-    }
-
-    function redraw() {
-    	ctx.clear();
-    	ctx.drawImage(p1img, player1.xpos, player1.ypos);
-    	ctx.drawImage(p1img, player2.xpos, player2.ypos);
-
-    }
-
-    function checkCollide() {
-
-    }
-
-    function GameLoop(argument) {
-        init();
-
-
-        //toDO
-        //flag carry
-        //capture flag check
-        //collide check
-
-
-
-    }
-}
